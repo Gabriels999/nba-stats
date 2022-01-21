0980b9319082311cc556dc50552cfba734529fdf
Gbeil <gab.tec97@gmail.com>
2022-01-21 19:49:38 -0300

    Implementando funções iniciais.

---
+++ assets/data.js
@@ -0,0 +1,569 @@
+let nbaLeague = {
+  conferencias: {
+    oeste: [
+      {
+        name: ' Philadelphia 76ers',
+        pos: 2,
+        players: [
+          'Tyrese Maxey',
+          'Seth Curry',
+          'Matisse Thybulle',
+          'Tobias Harris',
+          'Joel Embiid'
+        ],
+        ppg: 113.6,
+        w: 49,
+        l: 23
+      },
+      {
+        name: ' Brooklyn Nets',
+        pos: 2,
+        players: [
+          'James Harden',
+          'Kyrie Irving',
+          'DeAndre" Bembry',
+          'Kessler Edwards',
+          'Nic Claxton'
+        ],
+        ppg: 118.6,
+        w: 48,
+        l: 24
+      },
+      {
+        name: ' Milwaukee Bucks',
+        pos: 2,
+        players: [
+          'Jrue Holiday',
+          'Grayson Allen',
+          'Khris Middleton',
+          'Giannis Antetokounmpo',
+          'Bobby Portis'
+        ],
+        ppg: 120.1,
+        w: 46,
+        l: 26
+      },
+      {
+        name: ' Atlanta Hawks',
+        pos: 2,
+        players: [
+          'Trae Young',
+          'Bogdan Bogdanovic',
+          'De"Andre Hunter',
+          'John Collins',
+          'Clint Capela'
+        ],
+        ppg: 113.7,
+        w: 41,
+        l: 31
+      },
+      {
+        name: ' New York Knicks',
+        pos: 2,
+        players: [
+          'Kemba Walker',
+          'Evan Fournier',
+          'RJ Barrett',
+          'Julius Randle',
+          'Mitchell Robinson'
+        ],
+        ppg: 107.0,
+        w: 41,
+        l: 31
+      },
+      {
+        name: ' Miami Heat',
+        pos: 2,
+        players: [
+          'Kyle Lowry',
+          'Duncan Robinson',
+          'Jimmy Butler',
+          'P.J. Tucker',
+          'Bam Adebayo'
+        ],
+        ppg: 108.1,
+        w: 40,
+        l: 32
+      },
+      {
+        name: ' Boston Celtics',
+        pos: 2,
+        players: [
+          'Marcus Smart',
+          'Jaylen Brown',
+          'Jayson Tatum',
+          'Al Horford',
+          'Robert Williams III'
+        ],
+        ppg: 112.6,
+        w: 36,
+        l: 36
+      },
+      {
+        name: ' Washington Wizards',
+        pos: 2,
+        players: [
+          'Spencer Dinwiddie',
+          'Bradley Beal',
+          'Kentavious Caldwell-Pope',
+          'Kyle Kuzma',
+          'Daniel Gafford'
+        ],
+        ppg: 116.6,
+        w: 34,
+        l: 38
+      },
+      {
+        name: ' Indiana Pacers',
+        pos: 2,
+        players: [
+          'Malcolm Brogdon',
+          'Caris LeVert',
+          'Justin Holiday',
+          'Torrey Craig',
+          'Domantas Sabonis'
+        ],
+        ppg: 115.3,
+        w: 34,
+        l: 38
+      },
+      {
+        name: ' Charlotte Hornets',
+        pos: 2,
+        players: [
+          'LaMelo Ball',
+          'Terry Rozier',
+          'Gordon Hayward',
+          'Miles Bridges',
+          'Mason Plumlee'
+        ],
+        ppg: 109.5,
+        w: 33,
+        l: 39
+      },
+      {
+        name: ' Chicago Bulls',
+        pos: 2,
+        players: [
+          'Lonzo Ball',
+          'Zach LaVine',
+          'Javonte Green',
+          'DeMar DeRozan',
+          'Nikola Vucevic'
+        ],
+        ppg: 110.7,
+        w: 31,
+        l: 41
+      },
+      {
+        name: ' Toronto Raptors',
+        pos: 2,
+        players: [
+          'Fred VanVleet',
+          'Gary Trent Jr.',
+          'OG Anunoby',
+          'Scottie Barnes',
+          'Pascal Siakam'
+        ],
+        ppg: 111.3,
+        w: 27,
+        l: 45
+      },
+      {
+        name: ' Cleveland Cavaliers',
+        pos: 2,
+        players: [
+          'Darius Garland',
+          'Isaac Okoro',
+          'Lauri Markkanen',
+          'Evan Mobley',
+          'Jarrett Allen'
+        ],
+        ppg: 103.8,
+        w: 22,
+        l: 50
+      },
+      {
+        name: ' Orlando Magic',
+        pos: 2,
+        players: [
+          'Cole Anthony',
+          'Jalen Suggs',
+          'Franz Wagner',
+          'Wendell Carter Jr.',
+          'Mo Bamba'
+        ],
+        ppg: 104.0,
+        w: 21,
+        l: 51
+      },
+      {
+        name: ' Detroit Pistons',
+        pos: 2,
+        players: [
+          'Killian Hayes',
+          'Cade Cunningham',
+          'Hamidou Diallo',
+          'Saddiq Bey',
+          'Isaiah Stewart'
+        ],
+        ppg: 106.6,
+        w: 20,
+        l: 52
+      }
+    ],
+    leste: [
+      {
+        name: ' Phoenix Suns',
+        pos: 2,
+        players: [
+          'Chris Paul',
+          'Devin Booker',
+          'Mikal Bridges',
+          'Jae Crowder',
+          'Deandre Ayton'
+        ],
+        ppg: 115.3,
+        w: 51,
+        l: 21
+      },
+      {
+        name: ' Golden State Warriors',
+        pos: 14,
+        players: [
+          'Stephen Curry',
+          'Klay Thompson',
+          'Andrew Wiggins',
+          'Draymond Green',
+          'Kevon Looney'
+        ],
+        ppg: 113.7,
+        w: 39,
+        l: 33
+      },
+      {
+        name: ' Los Angeles Lakers',
+        pos: 9,
+        players: [
+          'Russel Westbrook',
+          'Malik Monk',
+          'Avery Bradley',
+          'Trevor Ariza',
+          'LeBron James'
+        ],
+        ppg: 109.5,
+        w: 42,
+        l: 30
+      },
+      {
+        name: ' Utah Jazz',
+        pos: 1,
+        players: [
+          'Mike Conley',
+          'Donovan Mitchell',
+          'Bojan Bogdanovic',
+          'Royce O"Neale',
+          'Rudy Gobert'
+        ],
+        ppg: 116.4,
+        w: 52,
+        l: 20
+      },
+      {
+        name: ' Denver Nuggets',
+        pos: 5,
+        players: [
+          'Monte Morris',
+          'Will Barton',
+          'Aaron Gordon',
+          'Jeff Green',
+          'Nikola Jokic'
+        ],
+        ppg: 115.1,
+        w: 47,
+        l: 25
+      },
+      {
+        name: ' LA Clippers',
+        pos: 6,
+        players: [
+          'Reggie Jackson',
+          'Luke Kennard',
+          'Marcus Morris Sr.',
+          'Nicolas Batum',
+          'Ivica Zubac'
+        ],
+        ppg: 114,
+        w: 47,
+        l: 25
+      },
+      {
+        name: ' Dallas Mavericks',
+        pos: 8,
+        players: [
+          'Luka Doncic',
+          'Jalen Brunson',
+          'Dorian Finney-Smith',
+          'Kristaps Porzingis',
+          'Maxi Kleber'
+        ],
+        ppg: 112.4,
+        w: 42,
+        l: 30
+      },
+      {
+        name: ' Portland Trail Blazers',
+        pos: 10,
+        players: [
+          'Anfernee Simons',
+          'CJ McCollum',
+          'Norman Powell',
+          'Larry Nance Jr.',
+          'Jusuf Nurkic'
+        ],
+        ppg: 116.1,
+        w: 42,
+        l: 30
+      },
+      {
+        name: ' Memphis Grizzlies',
+        pos: 15,
+        players: [
+          'Ja Morant',
+          'Desmond Bane',
+          'Ziaire Williams',
+          'Jaren Jackson Jr.',
+          'Steven Adams'
+        ],
+        ppg: 113.3,
+        w: 38,
+        l: 34
+      },
+      {
+        name: ' San Antonio Spurs',
+        pos: 19,
+        players: [
+          'Dejounte Murray',
+          'Derrick White',
+          'Doug McDermott',
+          'Keldon Johnson',
+          'Jakob Poeltl'
+        ],
+        ppg: 111.1,
+        w: 33,
+        l: 39
+      },
+      {
+        name: ' New Orleans Pelicans',
+        pos: 21,
+        players: [
+          'Devonte" Graham',
+          'Brandon Ingram',
+          'Josh Hart',
+          'Herbert Jones',
+          'Jonas Valanciunas'
+        ],
+        ppg: 114.6,
+        w: 31,
+        l: 41
+      },
+      {
+        name: ' Sacramento Kings',
+        pos: 23,
+        players: [
+          'De"Aaron Fox',
+          'Tyrese Haliburton',
+          'Harrison Barnes',
+          'Marvin Bagley III',
+          'Richaun Holmes'
+        ],
+        ppg: 113.7,
+        w: 31,
+        l: 41
+      },
+      {
+        name: ' Minnesota Timberwolves',
+        pos: 25,
+        players: [
+          'Patrick Beverley',
+          'D"Angelo Russell',
+          'Anthony Edwards',
+          'Jarred Vanderbilt',
+          'Karl-Anthony Towns'
+        ],
+        ppg: 112.1,
+        w: 23,
+        l: 49
+      },
+      {
+        name: ' Houston Rockets',
+        pos: 30,
+        players: [
+          'Kevin Porter Jr.',
+          'Jalen Green',
+          'Eric Gordon',
+          'Jae"Sean Tate',
+          'Christian Wood'
+        ],
+        ppg: 108.8,
+        w: 17,
+        l: 55
+      },
+      {
+        name: ' Oklahoma City Thunder',
+        pos: 27,
+        players: [
+          'Shai Gilgeous-Alexander',
+          'Josh Giddey',
+          'Luguentz Dort',
+          'Darius Bazley',
+          'Jeremiah Robinson-Earl'
+        ],
+        ppg: 105.1,
+        w: 22,
+        l: 50
+      }
+    ]
+  }
+}
+//===========================================LESTE=================================================
+let LesteWinChart = []
+for (i = 0; i < nbaLeague.conferencias.leste.length; i++) {
+  LesteWinChart.push(nbaLeague.conferencias.leste[i].w)
+}
+LesteWinChart.sort((a, b) => b - a)
+
+function LesteFirstPlace() {
+  let winner = LesteWinChart[0]
+  for (i = 0; i < nbaLeague.conferencias.leste.length; i++) {
+    if (nbaLeague.conferencias.leste[i].w == winner) {
+      console.log(
+        `O melhor colocado na conferência Leste foi: ${nbaLeague.conferencias.leste[i].name}.`
+      )
+    }
+  }
+}
+
+function LesteLastPlace() {
+  LesteWinChart.reverse()
+  let loser = LesteWinChart[0]
+  for (i = 0; i < nbaLeague.conferencias.leste.length; i++) {
+    if (nbaLeague.conferencias.leste[i].w == loser) {
+      console.log(
+        `O pior colocado na conferência Leste foi: ${nbaLeague.conferencias.leste[i].name}.`
+      )
+    }
+  }
+  LesteWinChart.reverse()
+}
+
+function LesteTopThreePlaces() {
+  let bestThree = []
+  let bestThreeNames = []
+
+  for (i = 0; i <= 2; i++) {
+    bestThree.push(LesteWinChart[i])
+  }
+  for (i = 0; i < bestThree.length; i++) {
+    for (j = 0; j < nbaLeague.conferencias.leste.length; j++) {
+      if (
+        nbaLeague.conferencias.leste[j].w == bestThree[i] &&
+        bestThreeNames.length < 3
+      ) {
+        bestThreeNames.push(nbaLeague.conferencias.leste[j].name)
+      }
+    }
+  }
+  console.log(`Os três melhores na conferência Leste foram:${bestThreeNames}.`)
+}
+function LesteTeams() {
+  console.log('Os times da conferência Leste são:')
+  for (i = 0; i < nbaLeague.conferencias.leste.length; i++) {
+    console.log(nbaLeague.conferencias.leste[i].name)
+  }
+}
+
+// LesteTopThreePlaces()
+// LesteFirstPlace()
+// LesteLastPlace()
+// LesteTeams()
+
+//===========================================OESTE=================================================
+
+let oesteWinChart = []
+for (i = 0; i < nbaLeague.conferencias.oeste.length; i++) {
+  oesteWinChart.push(nbaLeague.conferencias.oeste[i].w)
+}
+
+function OesteFirstPlace() {
+  let winner = oesteWinChart[0]
+  for (i = 0; i < nbaLeague.conferencias.oeste.length; i++) {
+    if (nbaLeague.conferencias.oeste[i].w == winner) {
+      console.log(
+        `O melhor colocado na conferência Oeste foi: ${nbaLeague.conferencias.oeste[i].name}`
+      )
+    }
+  }
+}
+
+function OesteTopThreePlaces() {
+  let bestThree = []
+  let bestThreeNames = []
+
+  for (i = 0; i <= 2; i++) {
+    bestThree.push(oesteWinChart[i])
+  }
+  for (i = 0; i < bestThree.length; i++) {
+    for (j = 0; j < nbaLeague.conferencias.oeste.length; j++) {
+      if (
+        nbaLeague.conferencias.oeste[j].w == bestThree[i] &&
+        bestThreeNames.length < 3
+      ) {
+        bestThreeNames.push(nbaLeague.conferencias.oeste[j].name)
+      }
+    }
+  }
+  console.log(
+    `Os três melhores colocados da conferência Oeste foram:${bestThreeNames}`
+  )
+}
+
+function OesteLastPlace() {
+  oesteWinChart.sort((a, b) => a - b)
+  let loser = oesteWinChart[0]
+  for (i = 0; i < nbaLeague.conferencias.oeste.length; i++) {
+    if (nbaLeague.conferencias.oeste[i].w == loser) {
+      console.log(
+        `O pior colocado na conferência Oeste foi:${nbaLeague.conferencias.oeste[i].name}`
+      )
+    }
+  }
+}
+
+function OesteTeams() {
+  console.log('Os times da conferência Oeste são:')
+  for (i = 0; i < nbaLeague.conferencias.oeste.length; i++) {
+    console.log(nbaLeague.conferencias.oeste[i].name)
+  }
+}
+// OesteFirstPlace()
+// OesteTopThreePlaces()
+// OesteLastPlace()
+// OesteTeams()
+
+//===============================================ALL===============================================
+
+function showPlayers(writePlayers) {
+  let nbaPlayers = []
+  for (i = 0; i < nbaLeague.conferencias.leste.length; i++) {
+    nbaPlayers.push(nbaLeague.conferencias.leste[i].players)
+  }
+  for (i = 0; i < nbaLeague.conferencias.oeste.length; i++) {
+    nbaPlayers.push(nbaLeague.conferencias.oeste[i].players)
+  }
+  for (i = 0; i < nbaPlayers.length; i++) {
+    writePlayers.innerHTML += ` ${nbaPlayers[i]}</br>`
+  }
+}
+let writePlayers = document.querySelector('#league_players')
+showPlayers(writePlayers)
